535c4510bd59a12b6f0c3c5670bfef612190997e
rBox1997 <mrbox2011@yandex.ru>
2016-09-24 11:59:45 +0000

    Sources

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdio.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <errno.h>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world" << endl;
+    
+	return 0;
+}
