5f9db21fdce6b6ece26c97bd0dc2622c26ce83a0
rBox1997 <mrbox2011@yandex.ru>
2016-09-24 15:13:51 +0000

    Data input implementation

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <dirent.h>
 #include <unistd.h>
+#include <vector>
 #include <sys/stat.h>
 #include <string.h>
 #include <errno.h>
@@ -9,9 +10,56 @@
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello world" << endl;
+// База данных студентов колледжа. Поля: ФИО, группа, 
+// признак бюджетности, стипендия (нет/обычная/повышенная),
+// флаг наличия социальной стипендии, дата рождения
+
+struct student {
+    string first_name;
+    string last_name;
+    string middle_name;
+    long birthdate;
+    string study_group;
+    bool has_paid_education;
+    bool has_social_grant;
+    char grant_type; // 0 - none, 1 - standart, 2 - extra
+};
+
+vector<student> all_students;
+
+void enter_field(string fieldname, string* field) {
+    cout << "Enter " << fieldname << ": ";
     
-	return 0;
+    string temp;
+    cin >> temp;
+    
+    *field = temp;
+}
+
+bool create_student(student* stdnt) {
+    string temp;
+    
+    cout << "Creating student." << endl;
+    
+    enter_field("first name", &stdnt->first_name);
+    enter_field("last name", &stdnt->last_name);
+    
+    string result;
+    cout << "Is data correct? ";
+    cin >> result;
+    
+    return result == "y" || result == "yes";
+}
+
+int main() {
+    student stdnt;
+    
+    if(create_student(&stdnt)) {
+        cout << "First name\t\t\tLast name" << endl;
+        cout << stdnt.first_name << "\t\t\t" << stdnt.last_name << endl;
+    } else {
+        cout << "Canceled." << endl;
+    }
+    
+    return 0;
 }
